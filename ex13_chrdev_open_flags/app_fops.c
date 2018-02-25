e85a7a5e47155d97fa01deca5fecf50b4b6c0b8c
goldage <thegreaterlee@gmail.com>
2018-02-25 18:44:40 +0800

    Use open-flags to solve competitive state.

---
+++ ex13_chrdev_open_flags/app_fops.c
@@ -0,0 +1,22 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+//#define PATHNAME "/dev/ex2"
+//#define PATHNAME "/dev/test_device2"
+#define PATHNAME "/dev/test_dev"
+
+int main(int argc , const char * argv[]){
+	int fd = -1;
+	fd = open(PATHNAME , O_RDWR);
+	if(fd < 0){
+		perror("open failed.\n");
+		return -1;
+	}
+
+	printf("open has done.\n");
+	while(1);
+	close(fd);
+	return 0;
+}
